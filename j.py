c75d80a6165ee6de0a7af52f89b9a5adb50cf499
tm <thulemon@proton.me>
2026-02-13 15:28:52 +0000

    added a cli

--- j.py
+++ j.py
@@ -134,43 +134,90 @@ async def fetch_resumes(
 
 LOCAL_DB_PATH = Path('store')
 async def main():
-    print('jw')
-    jdata = pl.read_parquet(LOCAL_DB_PATH / 'employees.parquet')
-    jrdata = pl.read_parquet(LOCAL_DB_PATH / 'resumes.parquet')
-
-    d = (
-        jdata.join(jrdata, 'resume').with_columns(
-            file_type=pl.when(pl.col.blob.bin.starts_with(b'%PDF'))
-                .then(pl.lit('.pdf'))
-                .when(pl.col.blob.bin.starts_with(b'PK'))
-                .then(pl.lit('.docx'))
-        )
+    import argparse
+    parser = argparse.ArgumentParser(
+        description="CLI with run and unpack subcommands."
     )
 
-    print(d)
-
-    d.write_parquet(
-        LOCAL_DB_PATH / "resumes",
-        partition_by=['vtype', 'file_type'],
+    subparsers = parser.add_subparsers(
+        dest="command",
+        required=True
     )
-    # d.write_parquet(
-    #     'resume_db.parquet',
-    # )
 
-    # for resume, blob, ft in d.select('resume', 'blob', 'file_type').iter_rows():
-    #     if blob is not None:
-    #         with open(f'resumes/{resume}{ft}', 'wb') as f:
-    #             f.write(blob)
-    #
-    exit(1)
+    run_parser = subparsers.add_parser(
+        "run",
+        help="Run the main process."
+    )
 
-    async with httpx.AsyncClient(timeout=60., follow_redirects=True) as client:
-        df = await fetch_resumes(client, jdata, jrdata)
+    run_parser.add_argument(
+        "-c", "--checkpoint",
+        choices=["restart", "use"],
+        required=True,
+        help="Checkpoint behavior: restart (rebuild resumes from source), or use (only attempt to get resumes not current checkpointed)."
+    )
+
+    up_parser = subparsers.add_parser(
+        "unpack",
+        help="Unpack resources."
+    )
+
+    up_parser.add_argument(
+        "-o", "--output-dir",
+        required=True,
+        help="where to unpack the resumes to",
+    )
+
+    args = parser.parse_args()
+
+    employees_path = (LOCAL_DB_PATH / 'employees.parquet')
+    checkpoint_path = (LOCAL_DB_PATH / 'resumes.parquet')
+    store_path = (LOCAL_DB_PATH / 'resumes')
+    match args.command:
+        case 'run':
+
+            async with httpx.AsyncClient() as client:
+                if employees_path.exists():
+                    emp_data = pl.read_parquet(employees_path)
+                else:
+                    print('getting employee data')
+                    emp_data = await rip_jroot_page(client)
+                    emp_data.write_parquet(employees_path)
 
 
-    df.write_parquet('./outr.parquet')
-    print(df)
+            checkpoint = None
+            if checkpoint_path.exists() and args.checkpoint == 'use':
+                checkpoint = pl.read_parquet(employees_path)
 
+            async with httpx.AsyncClient() as client:
+                resume_data = await fetch_resumes(client, emp_data, checkpoint)
+                resume_data.write_parquet(checkpoint_path)
+
+            output = (
+                emp_data.join(resume_data, 'resume').with_columns(
+                    file_type=pl.when(pl.col.blob.bin.starts_with(b'%PDF'))
+                        .then(pl.lit('.pdf'))
+                        .when(pl.col.blob.bin.starts_with(b'PK'))
+                        .then(pl.lit('.docx'))
+                )
+            )
+
+            output.write_parquet(store_path, partition_by=['vtype', 'file_type'])
+
+        case 'unpack':
+            assert employees_path.exists() and checkpoint_path.exists() and store_path.exists(), """
+run the 'run' subcommand first!
+"""
+            data = pl.read_parquet(store_path, hive_partitioning=True)
+
+
+            with_resumes = data.filter(pl.col.file_type.is_not_null())
+            print(f'{len(with_resumes)} / {len(data)} records with resumes, unpacking')
+
+            for n, r, blob, ft in with_resumes.select('name', 'resume', 'blob', 'file_type').iter_rows():
+                op = Path(args.output_dir)
+                op.mkdir(exist_ok=True)
+                with open(f'{args.output_dir}/{n.replace(' ', '_').lower()}_{r}{ft}', 'wb') as f:
+                    f.write(blob)
 
 
 if __name__ == "__main__":
